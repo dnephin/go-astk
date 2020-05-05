4fc02f72595a34e40c9ea6244122727bd7a6a4eb
Daniel Nephin <dnephin@gmail.com>
2020-05-04 21:50:40 -0400

    Add internal packages for testing

---
+++ internal/consumer/consumer.go
@@ -0,0 +1,36 @@
+package consumer
+
+import (
+	"fmt"
+
+	"github.com/dnephin/astprune/internal/target"
+)
+
+func symbolInReturnValue() *target.TypeSymbol1 {
+	return nil
+}
+
+func symbolInFuncArg(_ target.TypeSymbol2) {}
+
+type foo struct {
+	target.TypeSymbol3
+
+	symbolInStructField target.TypeSymbol4
+}
+
+var symbolInPkgVar *target.TypeSymbol5
+
+func symbolInFunctionBody() {
+	fmt.Println(target.VarSymbol)
+	target.FuncSymbol()
+
+	_ = target.VarSymbol
+	_ = target.ConstSymbol
+
+	var _ target.TypeSymbol8
+}
+
+type foop interface {
+	SymbolInInterfaceMethodReturn() target.TypeSymbol6
+	SymbolInInterfaceMethodArg(target.TypeSymbol7)
+}
---
+++ internal/target/target.go
@@ -0,0 +1,16 @@
+package target
+
+type TypeSymbol1 struct {}
+type TypeSymbol2 struct {}
+type TypeSymbol3 struct {}
+type TypeSymbol4 struct {}
+type TypeSymbol5 struct {}
+type TypeSymbol6 struct {}
+type TypeSymbol7 struct {}
+type TypeSymbol8 struct {}
+
+func FuncSymbol() {}
+
+var VarSymbol = "symbol"
+
+const ConstSymbol = "const"
